200fc0ee862927a83b863b9ad7e83d0cc880fbdc
TarCode <viktorprishchepa@yandex.ru>
2025-01-31 23:28:19 +0900

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,12 +1,6 @@
 import requests
-from IPython.display import HTML, display
 
 url = 'https://hdmn.cloud/ru/demo/'
-
-# Ссылки на GIF-анимации
-animated_checkmark_url = 'https://i.imgur.com/Uj3xPn5.gif'
-animated_cross_url = 'https://i.imgur.com/a7p5Nyb.gif'
-animated_warning_url = 'https://i.imgur.com/a7p5Nyb.gif'
 
 # Попытка получить страницу и проверить статус ответа
 try:
@@ -23,23 +17,13 @@ try:
             })
 
             if 'Ваш код выслан на почту' in response.text:
-                # Вывод сообщения с GIF-анимацией вместо обычного символа
-                html_message = f'<img src="{animated_checkmark_url}" alt="Animated Checkmark"> Ваш код уже в пути! Проверьте свой почтовый ящик.'
-                display(HTML(html_message))
+                print('✅ \033[1;32mВаш код уже в пути!\033[0m Проверьте свой почтовый ящик.')
             else:
-                # Замена ❌ на GIF-анимацию
-                html_message = f'<img src="{animated_cross_url}" alt="Animated Cross"> Указанная почта не подходит для получения тестового периода.'
-                display(HTML(html_message))
+                print('❌ \033[1;31mУказанная почта не подходит для получения тестового периода.\033[0m')
         else:
-            # Замена ⚠️ на GIF-анимацию
-            html_message = f'<img src="{animated_warning_url}" alt="Animated Warning"> На странице не найдено нужного текста. Проверьте доступность страницы.'
-            display(HTML(html_message))
+            print('⚠️ \033[1;31mНа странице не найдено нужного текста. Проверьте доступность страницы.\033[0m')
     else:
-        # Замена ⚠️ на GIF-анимацию
-        html_message = f'<img src="{animated_warning_url}" alt="Animated Warning"> Ошибка при запросе к странице. Код ответа: {response.status_code}'
-        display(HTML(html_message))
+        print(f"⚠️ \033[1;31mОшибка при запросе к странице.\033[0m Код ответа: {response.status_code}")
         
 except requests.RequestException as e:
-    # Замена ⚠️ на GIF-анимацию
-    html_message = f'<img src="{animated_warning_url}" alt="Animated Warning"> Ошибка при запросе к сайту: {e}'
-    display(HTML(html_message))
+    print(f"\033[1;31mОшибка при запросе к сайту:\033[0m {e}")
