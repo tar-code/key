e92c5b6ddb452669068f5f238a9fd5da86325712
TarCode <viktorprishchepa@yandex.ru>
2025-02-11 19:32:52 +0900

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,29 +1,40 @@
 import requests
+from IPython import get_ipython
 
-url = 'https://hdmn.cloud/ru/demo/'
+def main():
+    url = 'https://hdmn.cloud/ru/demo/'
 
-# Попытка получить страницу и проверить статус ответа
-try:
-    response = requests.get(url)
-    
-    # Проверка на успешный ответ от сервера
-    if response.status_code == 200:
-        # Если текст на странице содержит "Ваша электронная почта", продолжаем
-        if 'Ваша электронная почта' in response.text:
-            email = input('Введите электронную почту для получения тестового периода: ')
-
-            response = requests.post('https://hdmn.cloud/ru/demo/success/', data={
-                "demo_mail": f"{email}"
-            })
-
-            if 'Ваш код выслан на почту' in response.text:
-                print('✅ \033[1;32mВаш код уже в пути!\033[0m Проверьте свой почтовый ящик.')
-            else:
-                print('⚠️ \033[1;31mУказанная почта не подходит для получения тестового периода.\033[0m')
-        else:
-            print('⚠️ \033[1;31mНа странице не найдено нужного текста.\033[0m Проверьте доступность страницы.')
-    else:
-        print(f"❌ \033[1;31mОшибка при запросе к странице.\033[0m Код ответа: {response.status_code}")
+    try:
+        response = requests.get(url)
         
-except requests.RequestException as e:
-    print(f"❌ \033[1;31mОшибка при запросе к сайту:\033[0m {e}")
+        if response.status_code == 200:
+            if 'Ваша электронная почта' in response.text:
+                email = input('Введите электронную почту для получения тестового периода: ')
+                
+                response = requests.post('https://hdmn.cloud/ru/demo/success/', data={
+                    "demo_mail": f"{email}"
+                })
+
+                if 'Ваш код выслан на почту' in response.text:
+                    print('✅ Ваш код уже в пути! Проверьте свой почтовый ящик.')
+                    
+                    # Условие успешного завершения задачи
+                    restart_kernel()
+                else:
+                    print('⚠️ Указанная почта не подходит для получения тестового периода.')
+            else:
+                print('⚠️ На странице не найдено нужного текста. Проверьте доступность страницы.')
+        else:
+            print(f"❌ Ошибка при запросе к странице. Код ответа: {response.status_code}")
+            
+    except requests.RequestException as e:
+        print(f"❌ Ошибка при запросе к сайту: {e}")
+
+def restart_kernel():
+    ip = get_ipython()
+    if ip is not None:
+        ip.run_line_magic("reset", "-f")
+        print("Ядро перезапущено.")
+
+if __name__ == "__main__":
+    main()
